eaf6238b610a459f820c970f492566da219739b8
rasmus <svampen.elite@gmail.com>
2014-09-30 21:32:00 +0200

    Continues on both pot and potTest

--- src/texasholdem/Pot.java
+++ src/texasholdem/Pot.java
@@ -1,6 +1,18 @@
 package texasholdem;
 
-public class Pot
-{
+public class Pot{
+	
+	private int amount = 0;
 
+	public Pot(int amount){
+		if(amount < 1)
+			throw new IllegalArgumentException("The Pot has to be larger than 0");
+		
+		this.amount=amount;
+	}
+	
+	public int getSize(){
+		
+		return amount;
+	}
 }
