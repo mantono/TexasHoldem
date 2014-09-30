aad1b316980c1258c69f64b9423db4ceb52aa17e
Ellioteff <elliotfaltstrom@hotmail.com>
2014-09-30 20:33:08 +0200

    Added tests for the following methods: -getHand -newHand -addToPot -addChips
    Added the following methods to make the tests go through:
    
    -getHand
    -newHand
    -addToPot
    -addChips
    
    Also added the ArrayList hand to Player.java

--- test/PlayerTest.java
+++ test/PlayerTest.java
@@ -1,23 +1,82 @@
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import java.util.ArrayList;
 
 import org.junit.Test;
 
 import texasholdem.Player;
+import cards.Card;
+import cards.Colour;
+import cards.Rank;
 
 public class PlayerTest {
 
 	@Test
-	public void nameTest()
-	{
+	public void nameTest() {
 		Player player = new Player("kalle");
-		assertEquals("kalle",player.getName());
+		assertEquals("kalle", player.getName());
 	}
-	
+
 	@Test
-	public void potTest1(){
+	public void potTestIfIsZero() {
 		Player player = new Player("kalle");
-		assertEquals(0,player.getChips());
-		
-		
+		assertEquals(0, player.getChips());
+
 	}
+
+	@Test
+	public void getHandTestFromEmptyHand() {
+		Player player = new Player("kalle");
+		Card card1 = new Card(Colour.HEARTS, Rank.EIGHT);
+		Card card2 = new Card(Colour.HEARTS, Rank.ACE);
+		player.newHand(card1, card2);
+		ArrayList<Card> player1Cards = player.getHand();
+		assertTrue(player1Cards.contains(card1));
+		assertTrue(player1Cards.contains(card2));
+
+	}
+
+	@Test
+	public void getHandTestFromFullHand() {
+		Player player = new Player("kalle");
+		Card card1 = new Card(Colour.HEARTS, Rank.EIGHT);
+		Card card2 = new Card(Colour.HEARTS, Rank.ACE);
+		player.newHand(card1, card2);
+
+		Card card3 = new Card(Colour.SPADES, Rank.EIGHT);
+		Card card4 = new Card(Colour.CLUBS, Rank.THREE);
+		player.newHand(card3, card4);
+		ArrayList<Card> player1Cards = player.getHand();
+		assertFalse(player1Cards.contains(card1));
+		assertFalse(player1Cards.contains(card2));
+		assertTrue(player1Cards.contains(card3));
+		assertTrue(player1Cards.contains(card4));
+
+	}
+
+	@Test
+	public void addChipsTest() {
+		Player player = new Player("kalle");
+		player.addChips(200);
+		assertEquals(200, player.getChips());
+
+	}
+
+	@Test
+	public void addToPotTestSuccess() {
+		Player player = new Player("kalle");
+		player.addChips(200);
+		assertTrue(player.addToPot(150));
+	}
+
+	@Test
+	public void addToPottTestFail() {
+		Player player = new Player("kalle");
+		player.addChips(200);
+		assertFalse(player.addToPot(250));
+
+	}
+
 }
